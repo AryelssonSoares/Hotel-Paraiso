d339323baa8676abfab4f16a0c605f3f37f94053
AryelssonSoares <aryelssonsoares06@gmail.com>
2022-12-21 16:31:11 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function scrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-nav a[href^="#"]');
+
+    function scrollSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        
+        section.scrollIntoView ({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach(link => {
+        link.addEventListener('click', scrollSection);
+    });
+}
+scrollSuave();
+
+
+const sections = document.querySelectorAll('.js-scroll');
+
+const metadeWindow = window.innerHeight * 0.6;
+
+function animaScroll() {
+    sections.forEach(section => {
+        const topoSection = section.getBoundingClientRect().top;
+        const sectionVisible = (topoSection - metadeWindow) < 0;
+
+        if (sectionVisible) {
+            section.classList.add('ativo');
+        } else {
+            section.classList.remove('ativo');
+        }
+    });
+}
+animaScroll();
+window.addEventListener('scroll', animaScroll);
